f29f06db18ab77de8c57b9a4b6ce27c3c4cbf693
shifengyuan564 <shifengyuan564@126.com>
2017-09-22 15:09:48 +0800

    Merge branch 'master' of https://github.com/shifengyuan564/webpack-sample-project
    # Conflicts:
    #	demo-7-wind-coder/windcoder.iml

--- demo-6-springmvc/src/main/webapp/app/index.jsx
+++ demo-6-springmvc/src/main/webapp/app/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import { Router, useRouterHistory /*, browserHistory*/ } from 'react-router';
+import {Router, useRouterHistory /*, browserHistory*/} from 'react-router';
 import localizify from 'localizify';
-import { createHistory } from 'history';
+import {createHistory} from 'history';
 
 import en from './messages/en.json';
 import ru from './messages/ru.json';
@@ -11,17 +11,17 @@ import ru from './messages/ru.json';
 import routes from 'routers/routers.jsx';
 
 localizify
-  .add('en', en)
-  .add('ru', ru)
-  .setLocale(localStorage.locale || 'en');
+    .add('en', en)
+    .add('ru', ru)
+    .setLocale(localStorage.locale || 'en');
 
 // console.log(localizify.getLocale());
 
 const browserHistory = useRouterHistory(createHistory)({
-  basename: window.config.basename
+    basename: window.config.basename
 });
 
 ReactDOM.render(
-  <Router routes={routes} history={browserHistory}></Router>,
-  document.getElementById('app')
+    <Router routes={routes} history={browserHistory}></Router>,
+    document.getElementById('app')
 );
