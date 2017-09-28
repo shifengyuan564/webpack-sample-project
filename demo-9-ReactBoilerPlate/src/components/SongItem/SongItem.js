ae10d8cbad99cf944340327805346d448d47f1a2
shifengyuan564 <shifengyuan464>
2017-09-28 21:47:52 +0800

    Merge branch 'master' of https://github.com/shifengyuan564/webpack-sample-project
    # Conflicts:
    #	demo-9-ReactBoilerPlate/src/components/app.js

--- demo-9-ReactBoilerPlate/src/components/SongItem/SongItem.js
+++ demo-9-ReactBoilerPlate/src/components/SongItem/SongItem.js
@@ -2,7 +2,21 @@ import React, {PropTypes} from 'react';
 import styles from './SongItem.css';
 
 const SongItem = (props) => {
-    console.log(props);
+
+    const {songData} = props;
+    let reg_artist = /\[[^\]]+\]/g;
+
+    /* 截取[]之间的字符串*/
+    let artist = "[Bonne Aparte] Ta[st]e My Snow (3401)".match(reg_artist)[1];
+    artist = artist.substring(1, artist.length - 1);
+    console.log(artist);
+
+    /* 截取()之间的字符串*/
+    let reg_num = /\([^\)]+\)/g;
+    let num = "[Bonne Aparte] Ta[st]e My S(no)w (3401)".match(reg_num)[1];
+    num = num.substring(1, num.length - 1);
+    console.log(num);
+
     return (
         <div>
             SongItem
